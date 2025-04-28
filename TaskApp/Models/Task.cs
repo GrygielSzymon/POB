d42a51895296a129204ca83a318380ca08ac5623
Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2025-04-28 11:24:37 +0200

    Add files via upload

---
+++ TaskApp/Models/Task.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+    public enum Prioritylevel
+    {
+        Niski,
+        Średni,
+        Wysoki
+    }
+    public class Task
+    {
+        //prywatne pola
+        private string _title;
+        private string _description;
+        private DateTime _dueTime;
+        private bool _isCompleted;
+        private Prioritylevel priority;
+
+        //Właściwości z walidacją
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Tytuł zadania nie może byc pusty");
+                }
+                _title = value;
+            }
+
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Opis zadania nie może byc pusty");
+                }
+                _description = value;
+            }
+        }
+
+        public DateTime DueTime
+        {
+            get { return _dueTime; }
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new ArgumentException("Data wykonania zadania nie może być w przeszłości");
+                }
+                _dueTime = value;
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set { _isCompleted = value; }
+        }
+
+        public Prioritylevel Priority
+        {
+            get { return priority; }
+            set
+            {
+                if (!Enum.IsDefined(typeof(Prioritylevel), value))
+                {
+                    throw new ArgumentException($"Priorytet: '{value}' nie jest dozwolony. Dozwolone priorytety: {string.Join(",", Enum.
+                        GetNames(typeof(Prioritylevel)))}");
+                }
+            }
+        }
+
+        //Konstruktor domyślny
+        public Task()
+        {
+            _title = "Nieokreślony tytuł";
+            _description = "Brak opisu";
+            _dueTime = DateTime.Now.AddDays(1);
+            _isCompleted = false;
+            priority = Prioritylevel.Średni;
+        }
+
+        //Konstruktor parametryczny
+        public Task(string title, string description, DateTime dueTime, bool isCompleated, Prioritylevel priority)
+        {
+            _title = title;
+            _description = description;
+            _dueTime = dueTime;
+            _isCompleted = isCompleated;
+            Priority = priority;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Zadanie: {Title}\n Opis: {Description}\n Termin wykonania: {DueTime}\n Wykonane: {IsCompleted}\n " +
+                $"Priorytet: {Priority}");
+        }
+
+    }
+}
---
+++ TaskApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using TaskApp.Models;
+
+namespace TaskApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 1
+            TaskApp.Models.Task task1 = new TaskApp.Models.Task("zadanie 1", "opis zadania", DateTime.Now.AddDays(5), true, Prioritylevel.Średni);
+            task1.DueTime = DateTime.Now.AddDays(2);
+            task1.IsCompleted = false;
+            task1.DisplayInfo();
+
+            //zadanie 2
+            TaskApp.Models.Task task2 = new TaskApp.Models.Task("zadanie 2", "opis zadania", DateTime.Now.AddDays(5), true, Prioritylevel.Wysoki);
+            task2.DisplayInfo();
+
+            // Próba ustawienia daty w przeszłości
+            try
+            {
+                task2.DueTime = DateTime.Now.AddDays(-5);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+
+            // Poprawne przesunięcie daty o 5 dni do przodu
+            task2.DueTime = task2.DueTime.AddDays(5);
+            task2.DisplayInfo();
+
+            try
+            {
+                task2.Priority = Prioritylevel.Niski;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+
+            task2.DisplayInfo();
+
+        }
+    }
+}
